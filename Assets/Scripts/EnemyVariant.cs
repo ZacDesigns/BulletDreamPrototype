4e461d9a98e392df70b4044339a7dd777a900fd1
Zac <zacmaibusiness@gmail.com>
2021-06-14 20:51:42 +1000

    Nav Mesh Update for enemy movement, weapon tweaks

--- Assets/Scripts/EnemyVariant.cs
+++ Assets/Scripts/EnemyVariant.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyVariant : MonoBehaviour
 {
-    public float speed; 
+    public float speed;
+
+    public float playerDetected = 12;
+    public GameObject player;
+    private NavMeshAgent navAgent;
 
 
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
 
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, player.transform.position) < playerDetected)
+        {
+            navAgent.destination = player.transform.position;
+        }
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,7 +33,7 @@ public class EnemyVariant : MonoBehaviour
         if (other.tag == "Player")
         {
             Debug.Log("Player Dead!!");
-
+    
         }
         if (other.tag == "Player Projectile")
         {
