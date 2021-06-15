7be85ded64d481e366bc7f6430e09d7f97bb53a4
Zac <zacmaibusiness@gmail.com>
2021-06-15 12:29:34 +1000

    Main Menu functionality added, Pause screen layout created

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseScreen;
+
+    [HideInInspector]
+    public bool isPaused = false;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+
+
+}
---
+++ Assets/Scripts/ReturnMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenu : MonoBehaviour
+{
+    public void buttonReturnMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
