36ac91638279a1d84ab83a517cdbda6ae7d5a70a
Zac <zacmaibusiness@gmail.com>
2021-06-19 14:56:26 +1000

    Score tally and Timer added

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text textTimer;
+    private float startTimer;
+
+
+
+    void Start()
+    {
+        startTimer = Time.time;
+
+
+    }
+
+
+    void Update()
+    {
+        float timeCont = Time.time - startTimer;
+
+        string minutes = ((int)timeCont / 60).ToString();
+        string seconds = (timeCont % 60).ToString("f2");
+
+        textTimer.text = minutes + ":" + seconds;
+
+
+
+    }
+}
