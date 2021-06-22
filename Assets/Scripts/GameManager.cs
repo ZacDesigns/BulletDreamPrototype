962c59dc6390e29d3d7fc0bbd74e1dee89339a58
Zac <zacmaibusiness@gmail.com>
2021-06-23 00:47:32 +1000

    Comments added to GameManager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,23 +6,25 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //Variable, Component and Game Object References
     public GameObject GameOverScreen;
     public static bool gameIsOver = false;
-
     public Score score;
-    
     public Text scoreGameOver;
 
-
+    // Start is called before the first frame update
     void Start()
     {
+        //Getting the component of type Score and deactivating the game over screen
         score.GetComponent<Score>();
         GameOverScreen.SetActive(false);
     }
+    //This function checks if gameIsOver is equal to false, then activates game over screen
     public void GameOver()
     {
         if (gameIsOver == false)
         {
+            //Sets the gameIsOver value to true and sets up the rest of the game over screen
             gameIsOver = true;
             GameOverScreen.SetActive(true);
             Cursor.visible = true;
