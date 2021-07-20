43da9818ad4ccc83cc4f52211624d9ab94e767eb
Zac <zacmaibusiness@gmail.com>
2021-07-20 15:21:44 +1000

    Button Select Implementation

---
+++ Assets/Scripts/SoundSelectStore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSelectStore : MonoBehaviour
+{
+
+    private static SoundSelectStore selectSoundInstance;
+    private GameObject sfxButtonSelect;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (selectSoundInstance == null)
+        {
+            selectSoundInstance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+}
